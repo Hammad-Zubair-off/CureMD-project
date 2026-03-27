36a64c33e3fe55bf5e53e60823a7d72dc647f688
Ravindu Thiranjaya <thiranjayaravindu@gmail.com>
2026-03-27 17:21:05 +0530

    feat: set up main application routing with protected routes

---
+++ frontend/src/App.jsx
@@ -0,0 +1,46 @@
+import './App.css'
+import { Route, Routes } from 'react-router-dom'
+import LandingPage from './pages/LandingPage'
+import LoginPage from './pages/LoginPage'
+import RegisterPage from './pages/RegisterPage'
+import Dashboard from './pages/Dashboard'
+import { ProtectedRoute } from './components/ProtectedRoute'
+import AdminDashboard from './pages/AdminDashboard'
+
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<LandingPage />} />
+      <Route path="/login" element={<LoginPage />} />
+      <Route path="/register" element={<RegisterPage />} />
+
+      {/* Protected Routes */}
+      <Route
+        path="/dashboard"
+        element={
+          <ProtectedRoute>
+            <Dashboard />
+          </ProtectedRoute>
+        }
+      />
+      <Route
+        path="/admin"
+        element={
+          <ProtectedRoute requiredRole="admin">
+            <AdminDashboard />
+          </ProtectedRoute>
+        }
+      />
+      {/* <Route
+        path="/admin/superadmin"
+        element={
+          <ProtectedRoute requiredRole="superadmin">
+            <AdminDashboard />
+          </ProtectedRoute>
+        }
+      /> */}
+    </Routes>
+  )
+}
+
+export default App
